73104513759059ed84fe11ee22b9d17494c0cff6
Gaein nidb <mail@gaein.cn>
2022-12-25 00:24:24 +0800

    Add frontend and navbar

---
+++ src/frontend/course_evaluation/src/util/menu.js
@@ -0,0 +1,12 @@
+export default {
+    menu: [{
+        icon: "mdi-home",
+        title: "主页",
+        url: "/"
+    }, {
+        icon: "mdi-account",
+        title: "账号",
+        url: "/account"
+    }]
+
+}
